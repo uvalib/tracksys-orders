d1fa2694162bbe89f3a9fa17edc7ef01d91558b9
Lou Foster <louffoster@gmail.com>
2026-06-09 10:18:12 -0400

    non-uva email for existing patrons is disabled by default. added a change button to it can be updated

--- frontend/src/components/steps/CustomerInfo.vue
+++ frontend/src/components/steps/CustomerInfo.vue
@@ -3,24 +3,32 @@
       <FormKit label="First Name" type="text" v-model="orderStore.customer.firstName" validation="required"  id="fname" autofocus/>
       <FormKit label="Last Name" type="text" v-model="orderStore.customer.lastName" validation="required"  id="lname"/>
       <template  v-if="orderStore.computeID.length > 0">
-         <FormKit label="Email" type="email" v-model="orderStore.customer.email" :disabled="true"
-            help="If multiple email recipients are needed, please note them in the 'Special Instructions' section of the 'Request Info' step."
-         />
+         <div>
+            <FormKit label="Email" type="email" v-model="orderStore.customer.email" :disabled="true"/>
+            <div class="help">If multiple email recipients are needed, please note them in the 'Special Instructions' section of the 'Request Info' step.</div>
+         </div>
          <FormKit type="select" label="Academic Status" v-model="orderStore.customer.academicStatusID"
             placeholder="Select an academic status" :options="academicStatuses" validation="required"
          />
       </template>
-      <FormKit v-else label="Email" type="email" v-model="orderStore.customer.email" 
-         validation="required|email" validation-visibility="live" id="email" 
-         help="If multiple email recipients are needed, please note them in the 'Special Instructions' section of the 'Request Info' step."
-      />
+      <div v-else >
+         <div class="email-entry">
+            <FormKit label="Email" type="email" v-model="orderStore.customer.email" 
+               validation="required|email" validation-visibility="live" id="email" :disabled="orderStore.customer.id >0 && !editEmail"
+            />
+            <button v-if="orderStore.customer.id >0 && !editEmail" @click="editEmail=true">Change</button>
+         </div>
+         <div class="help">If multiple email recipients are needed, please note them in the 'Special Instructions' section of the 'Request Info' step.</div>
+      </div>
    </FormKit>
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { useOrderStore } from '@/stores/order'
 
+
+const editEmail = ref(false)
 const orderStore = useOrderStore()
 
 const academicStatuses = computed(()=>{
@@ -41,4 +49,33 @@ async function beforeStepChange({currentStep}) {
 </script>
 
 <style scoped lang="scss">
+.help {
+   margin-top: 10px;
+   font-size: 0.95em;
+}
+.email-entry {
+   display: flex;
+   flex-flow: row nowrap;
+   gap: 10px;
+   justify-content: flex-start;
+   align-items: flex-end;
+   .formkit-outer {
+      flex-grow: 1;
+   }
+   button {
+      white-space: nowrap;
+      border-radius: 0.3rem;
+      border:1px solid #ccccd7;
+      cursor: pointer;
+      padding: 7px 10px;
+      transition: background-color 0.25s ease-in-out;
+      &:hover {
+         background-color: #ddddd7;
+      }
+      &:focus {
+         outline: 2px dotted var( --uvalib-accessibility-highlight );
+         outline-offset: 3px;
+      }
+   }
+}
 </style>
